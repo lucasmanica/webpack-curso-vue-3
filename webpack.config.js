a9d9035480106da1e4a811a08321062461b80870
Lucas Manica <lucas.manica@brasil317.com.br>
2023-01-31 15:34:13 -0300

    init: New Project Vue

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const { vueLoaderPlugin } = require("vue-loader");
+const MiniCssExtractPlugin = requerie("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+};
